8d75ef469848f1a322bb65be25b6b2787579405e
khushwinder <chahalkhushwinder@gmail.com>
2022-02-22 21:39:55 -0500

    fixing responsive design issue

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -7,10 +7,19 @@ const MainHero = () => {
   return (
     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <div className="sm:text-center lg:text-left">
-        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block xl:inline">{mainHero.title}</span>{' '}
+        <h1
+          className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
+        >
+          {mainHero.title.split(' ').map((word, index) => (
+            <span
+              key={index}
+              className={index % 2 ? 'text-primary' : 'text-border'}
+            >
+              {word}{' '}
+            </span>
+          ))}
         </h1>
-        <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+        <p className={`text-gray-600`} id="root">
           {mainHero.description}
         </p>
       </div>
