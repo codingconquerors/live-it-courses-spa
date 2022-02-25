5f76c74a2853d623e9d1568772d8732783da258b
khushwinder <chahalkhushwinder@gmail.com>
2022-02-24 22:00:39 -0500

    fixing contents

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
-        <title>Online IT course with live classes</title>
+        <title>Online IT courses with live classes | </title>
         <meta name="title" content="Online IT courses with live classes" />
         <meta
           name="description"
-          content="Learn spring boot, java based online courses from professionals in live classess."
+          content="Learn online IT courses in live classes. We teach online live courses based on java, spring boot, apache camel, kafka, microservices, docker, kubernetes and other open source technologies. Click contact us link to know more."
         />
         <meta name="url" content="https://www.codingconquerors.com/" />
         <meta name="type" content="website" />
         <meta name="robots" content="index, follow" />
+        <meta
+          httpEquiv="Content-Type"
+          content="text/html; charset=utf-8"
+        ></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
